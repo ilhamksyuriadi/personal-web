47ee080ad1d91c3da5859db712d568159d9f39a6
ilham <ilhamksyuriadi@gmail.com>
2020-07-21 02:33:35 +0700

    test syntax

---
+++ src/Home/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './home.css';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Ini Home Page</h1>
+            </div>
+        )
+    }
+}
